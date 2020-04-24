7c3ae8177c36406f6bd470744c6d12eb1da111ee
Ausaaf Nabi <nabiausaaf@gmail.com>
2020-04-24 21:45:46 +0530

    Add files via upload

---
+++ src/Test.java
@@ -0,0 +1,13 @@
+class Test{
+    public static void main(String[] args){
+        System.out.println("hello World");
+        stack();
+    }
+    public static void stack(){
+        System.out.println("##Initialize the Stack and create features for that");
+        int a=1,b=4;
+        if(a!=b){
+            System.out.println("test Completed");
+        }
+    }
+}
